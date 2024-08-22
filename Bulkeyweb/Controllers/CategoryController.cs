77f38849843679655a9e0bcb0becb8d4f84c79df
TASEEN MOHAMMAD <115091557+TASEEN1@users.noreply.github.com>
2024-08-22 17:22:20 +0600

    bacis curd

--- Bulkeyweb/Controllers/CategoryController.cs
+++ Bulkeyweb/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Bulkeyweb.Data;
 using Bulkeyweb.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
 
 namespace Bulkeyweb.Controllers
 {
@@ -25,10 +26,76 @@ namespace Bulkeyweb.Controllers
         [HttpPost]
         public IActionResult CreateCategory(Category obj)
         {
-            _db.Categoris.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _db.Categoris.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
 
         }
+        public IActionResult EditCategory(int ID)
+        {
+            if(ID == null|| ID==0)
+            {
+                return NotFound();
+            }
+            Category categoryformDb = _db.Categoris.Find(ID);
+            if (categoryformDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryformDb);
+
+        }
+        [HttpPost]
+        public IActionResult EditCategory(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categoris.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+
+        }
+
+        public IActionResult DeleteCategory(int? ID) // Changed int to int?
+        {
+            if (ID == null || ID == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _db.Categoris.Find(ID);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteCategoryPost(int ID)
+        {
+            var obj = _db.Categoris.Find(ID);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categoris.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
+
 }
--- Bulkeyweb/Models/Category.cs
+++ Bulkeyweb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Bulkeyweb.Models
 {
@@ -7,7 +8,11 @@ namespace Bulkeyweb.Models
         [Key]
         public int ID { get; set; }
         [Required]
+        [DisplayName("Category Name")]
+        [MaxLength(30)]
         public string? Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100)]
         public string? DisplayOrder { get; set; }
     }
 }
